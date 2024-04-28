027dbd52e580891e48a88173cd6811397db93291
Runsprint <gvantsakapanadze95@gmail.com>
2024-04-28 19:54:52 +0400

    html/css

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
 
-document.getElementById('next-button2').addEventListener('click', function() {
-  window.location.href = "../Third/index.html";
-  console.log("something")
-});
-document.getElementById('next-button1').addEventListener('click', function() {
-  window.location.href = "./Second/index.html";
-});
