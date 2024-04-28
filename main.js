79cf71998eee140fc7bf1762b4758137469e40a5
Runsprint <gvantsakapanadze95@gmail.com>
2024-04-28 12:10:54 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+document.getElementById('next-button2').addEventListener('click', function() {
+  window.location.href = "../Third/index.html";
+  console.log("something")
+});
+document.getElementById('next-button1').addEventListener('click', function() {
+  window.location.href = "./Second/index.html";
+});
