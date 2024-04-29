d349737610b5855ff26a05250a60b769dc4ab763
Runsprint <gvantsakapanadze95@gmail.com>
2024-04-29 16:59:59 +0400

    without responsive

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 
+function handleClick(event) {
+    let clickInfo = JSON.parse(localStorage.getItem('clickInfo')) || [];
+  
+    let clickedId = event.target.getAttribute('id');
+    clickInfo.push(clickedId);
+    localStorage.setItem('clickInfo', JSON.stringify(clickInfo));
+  //url
+    let currentUrl = window.location.href;
+    let newUrl = currentUrl + '?clicked=' + clickedId;
+    window.history.pushState({}, '', newUrl);
+  
+    if(clickedId){
+        console.log(clickedId)
+    }
+
+  }
+  
+  let check1 = document.getElementById('1');
+  let check2 = document.getElementById('2');
+  let check3 = document.getElementById('3');
+  let check4 = document.getElementById('4');
+  
+  check1.addEventListener('click', handleClick);
+  check2.addEventListener('click', handleClick);
+  check3.addEventListener('click', handleClick);
+  check4.addEventListener('click', handleClick);
+  
+  
+
+  
